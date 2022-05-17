c965202187ca1bfffc36337f3c8b0362d6807210
mpapenbr <Markus.Papenbrock+github@gmail.com>
2022-05-17 14:08:10 +0000

    ref: split github/bitbucket processing in own methods

--- releaseupdater/process_changes.go
+++ releaseupdater/process_changes.go
@@ -23,77 +23,98 @@ func ProcessNewRelease(ctx Context, release *github.ReleaseEvent) {
 			for _, update := range action.Update {
 				log.Printf("%v\n", update)
 				repoOwner := *release.Repo.Owner.Login
-				for _, toUpdateFile := range update.Files {
-					fmt.Printf("RepoType: <%s> <%s>\n", update.RepoType, strings.ToLower(update.RepoType))
-					log.Printf("Fetching %s from %s/%s\n", toUpdateFile, repoOwner, update.Repo)
-					content, _, resp, err := ctx.ProbotCtx.GitHub.Repositories.GetContents(
-						context.Background(),
-						repoOwner,
-						update.Repo,
-						toUpdateFile,
-						&github.RepositoryContentGetOptions{})
-					if err != nil {
-						log.Printf("error reading source: %+v\n", err)
-						continue
-					}
-					fileContent, _ := b64.StdEncoding.DecodeString(*content.Content)
-					log.Printf("Content: <%s>\n", fileContent)
-					log.Printf("RegEx: <%s>\n", update.Regex)
-					log.Printf("Resp: %+v\n", *resp)
-					newVersion := ReplaceVersion(fileContent, update.Regex, *release.Release.TagName)
-					if string(newVersion) != string(fileContent) {
-						fmt.Printf("Updating file %s\n", toUpdateFile)
-						fmt.Printf("NewContent: <%s>\n", string(newVersion))
-
-						message := fmt.Sprintf("pkg: Bump %s to %s", *commitComponent, *release.Release.TagName)
-
-						switch strings.ToLower(update.RepoType) {
-						case "bitbucket":
-							err = updateToBitbucket(*ctx.BitbucketClient, string(newVersion), update.Repo, toUpdateFile, message)
-						default:
-							_, resp, err = ctx.ProbotCtx.GitHub.Repositories.UpdateFile(
-								context.Background(),
-								repoOwner,
-								update.Repo,
-								toUpdateFile,
-								&github.RepositoryContentFileOptions{
-									Content: []byte(newVersion),
-									Message: github.String(message),
-									SHA:     github.String(*content.SHA),
-								})
-						}
-						if err != nil {
-							log.Printf("error updating %s: %+v\n", toUpdateFile, err)
-							continue
-						}
-
-					} else {
-						log.Println("No changes detected")
-					}
-
+				replacer := func(content string) (string, string) {
+					return ReplaceVersionString(content, update.Regex, *release.Release.TagName),
+						fmt.Sprintf("pkg: Bump %s to %s", *commitComponent, *release.Release.TagName)
 				}
+				switch strings.ToLower(update.RepoType) {
+				case "bitbucket":
+					handleBitbucket(ctx, update, replacer)
+				default:
+					handleGithub(ctx, repoOwner, update, replacer)
+				}
+
 			}
 		}
 	}
 }
 
-func updateToBitbucket(client bitbucket.Client, content string, repo string, filename string, message string) error {
-	f, _ := os.CreateTemp("", "bbupload")
-	f.WriteString(content)
-	f.Close()
+func handleGithub(ctx Context, repoOwner string, update Update, processContent func(content string) (string, string)) {
+	for _, toUpdateFile := range update.Files {
+		content, _, resp, err := ctx.ProbotCtx.GitHub.Repositories.GetContents(
+			context.Background(),
+			repoOwner,
+			update.Repo,
+			toUpdateFile,
+			&github.RepositoryContentGetOptions{})
+		if err != nil {
+			log.Printf("error reading source: %+v\n", err)
+			continue
+		}
+		fileContent, _ := b64.StdEncoding.DecodeString(*content.Content)
+		log.Printf("Content: <%s>\n", fileContent)
+		log.Printf("RegEx: <%s>\n", update.Regex)
+		log.Printf("Resp: %+v\n", *resp)
+		newVersion, message := processContent(string(fileContent))
+		if string(newVersion) != string(fileContent) {
+			fmt.Printf("Updating file %s\n", toUpdateFile)
+			fmt.Printf("NewContent: <%s>\n", string(newVersion))
 
-	retErr := client.Repositories.Repository.WriteFileBlob(&bitbucket.RepositoryBlobWriteOptions{
-		Owner:    "mpapenbr",
-		RepoSlug: repo,
-		FilePath: f.Name(),
-		FileName: filename,
-		Branch:   "master",
-		Message:  message,
-	})
-	log.Printf("Deleting temp file %s\n", f.Name())
-	err := os.Remove(f.Name())
-	if err != nil {
-		log.Printf("Error deleting temp file %s: %v\n", f.Name(), err)
+			_, resp, err = ctx.ProbotCtx.GitHub.Repositories.UpdateFile(
+				context.Background(),
+				repoOwner,
+				update.Repo,
+				toUpdateFile,
+				&github.RepositoryContentFileOptions{
+					Content: []byte(newVersion),
+					Message: github.String(message),
+					SHA:     github.String(*content.SHA),
+				})
+
+			if err != nil {
+				log.Printf("error updating %s: %+v\n", toUpdateFile, err)
+				continue
+			}
+		} else {
+			log.Println("No changes detected")
+		}
 	}
-	return retErr
+}
+
+func handleBitbucket(ctx Context, update Update, processContent func(content string) (string, string)) {
+	for _, toUpdateFile := range update.Files {
+		fileContent, err := ctx.BitbucketClient.Repositories.Repository.GetFileBlob(
+			&bitbucket.RepositoryBlobOptions{Owner: "mpapenbr",
+				RepoSlug: update.Repo, Path: toUpdateFile, Ref: "master"})
+		if err != nil {
+			log.Printf("error reading source: %+v\n", err)
+			continue
+		}
+		log.Println(fileContent.String())
+		newVersion, message := processContent(string(fileContent.String()))
+		if string(newVersion) != string(fileContent.String()) {
+			fmt.Printf("Updating file %s\n", toUpdateFile)
+			fmt.Printf("NewContent: <%s>\n", string(newVersion))
+
+			f, _ := os.CreateTemp("", "bbupload")
+			f.WriteString(newVersion)
+			f.Close()
+
+			err := ctx.BitbucketClient.Repositories.Repository.WriteFileBlob(&bitbucket.RepositoryBlobWriteOptions{
+				Owner:    "mpapenbr",
+				RepoSlug: update.Repo,
+				FilePath: f.Name(),
+				FileName: toUpdateFile,
+				Branch:   "master",
+				Message:  message,
+			})
+			log.Printf("Deleting temp file %s\n", f.Name())
+			err = os.Remove(f.Name())
+			if err != nil {
+				log.Printf("Error deleting temp file %s: %v\n", f.Name(), err)
+			}
+		} else {
+			log.Println("No changes detected")
+		}
+	}
 }
