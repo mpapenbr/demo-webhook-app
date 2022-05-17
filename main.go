6c016fae1bf24df754c6ebd169f107be031b4aed
mpapenbr <Markus.Papenbrock+github@gmail.com>
2022-05-17 11:50:53 +0000

    feat: bitbucket access, multiple files in update

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
+	"os"
 
 	"demo-webhook-app/releaseupdater"
 
 	"github.com/google/go-github/v44/github"
+	"github.com/ktrysmt/go-bitbucket"
 	"github.com/mpapenbr/go-probot/probot"
 )
 
@@ -17,20 +17,6 @@ func main() {
 		log.Fatal("Could not read config")
 	}
 
-	probot.HandleEvent("create", func(ctx *probot.Context) error {
-		// Because we're listening for "release" we know the payload is a *github.ReleaseEvent
-		event := ctx.Payload.(*github.CreateEvent)
-		if *event.RefType == "tag" {
-			log.Printf("got create tag from %s\n", *event.GetRepo().Name)
-			releaseupdater.ProcessNewTag(config, ctx, event)
-
-		} else {
-			log.Printf("not interested in ref_type %s\n", *event.RefType)
-		}
-
-		return nil
-	})
-
 	probot.HandleEvent("ping", func(ctx *probot.Context) error {
 		log.Printf("Ping event recieved\n")
 		return nil
@@ -39,35 +25,12 @@ func main() {
 	probot.HandleEvent("release", func(ctx *probot.Context) error {
 		// Because we're listening for "release" we know the payload is a *github.ReleaseEvent
 		event := ctx.Payload.(*github.ReleaseEvent)
-		log.Printf("Currently ignoring release events (%s)\n", *event.Action)
-		return nil
-	})
-
-	probot.HandleEvent("release_ooo", func(ctx *probot.Context) error {
-		// Because we're listening for "release" we know the payload is a *github.ReleaseEvent
-		event := ctx.Payload.(*github.ReleaseEvent)
 		if *event.Action == "published" {
-
+			c := bitbucket.Client(*bitbucket.NewBasicAuth(os.Getenv("BITBUCKET_APP_USER"), os.Getenv("BITBUCKET_APP_PASSWORD")))
+			localContext := releaseupdater.Context{Config: config, ProbotCtx: ctx, BitbucketClient: &c}
 			// log.Printf("got release published %+v\n", event)
 			log.Printf("got release published from %s\n", *event.GetRepo().Name)
-			releaseupdater.ProcessNewRelease(config, ctx, event)
-			repoOwner := "mpapenbr"
-			repo := "demo_deploy1"
-			fileRef := "versions.properties"
-			content, _, resp, err := ctx.GitHub.Repositories.GetContents(context.Background(), repoOwner, repo, fileRef, &github.RepositoryContentGetOptions{})
-			_, resp, err = ctx.GitHub.Repositories.UpdateFile(context.Background(), repoOwner, repo, fileRef, &github.RepositoryContentFileOptions{
-				Content: []byte("demo_app_version1: " + *event.Release.TagName),
-				Message: github.String("changed by bot for " + *event.Release.TagName),
-				SHA:     github.String(*content.SHA),
-			})
-			if err != nil {
-
-				log.Fatalf("UpdateFileRest %v", err)
-			}
-
-			if false {
-				fmt.Printf("%v", resp)
-			}
+			releaseupdater.ProcessNewRelease(localContext, event)
 		} else {
 			log.Printf("I'm only interested in published releases.\n")
 		}
